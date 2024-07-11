9ccfb9c4fbd665ab861fc309526f0e6fac39f2b9
LENOVO <LENOVO@LAPTOP-97USSKU1>
2024-07-11 15:48:51 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace DienTichHinhChuNhat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap chieu dai: ");
+            BigInteger ChieuDai = BigInteger.Parse(Console.ReadLine());
+            Console.Write("Nhap chieu rong: ");
+            BigInteger ChieuRong = BigInteger.Parse(Console.ReadLine());
+            BigInteger DienTich = ChieuDai * ChieuRong;
+            Console.WriteLine("Dien tich hinh chu nhat la: " + DienTich);
+        }
+    }
+}
